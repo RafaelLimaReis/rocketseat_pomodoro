90dfbe14f204d759be286b24c9ef38cfa193d15b
Rafael Reis <rafaelreis@gurgelmix.com.br>
2024-02-01 21:22:52 -0300

    iniciando config de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
