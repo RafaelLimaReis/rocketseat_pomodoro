489bbc20dc11509e43c9585f27ebca0b34679253
Rafael Reis <rafaelreis@gurgelmix.com.br>
2024-02-03 00:51:58 -0300

    integrando form com ts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,12 +18,20 @@ const newCycleFormValidationSchema = zod.object({
   minutesAmount: zod.number().min(5).max(60),
 })
 
+type InterfaceNewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<InterfaceNewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle() {}
+  function handleCreateNewCycle(data: InterfaceNewCycleFormData) {
+    console.log(data)
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
